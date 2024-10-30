ead01be6974005bd7d28853b2b1dac64520e3d66
Dulshad Faraj <dulshadf@gmail.com>
2024-10-30 22:31:43 +0100

    small update

--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -1,17 +1,27 @@
 import styles from "./Meme.module.css"
 import memesData from "./../../memesData.js"
-import { useState } from 'react'
+import {useState} from 'react'
 
 const Meme = () => {
 
-    const [memeImage, setMemeImage] = useState("")
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function getRandomMemeImage() {
-        const memesArray = memesData.data.memes;
+        const memesArray = allMemeImages.data.memes;
         const randomIndex = Math.floor(Math.random() * memesArray.length);
         const memeImageUrl = memesArray[randomIndex].url;
 
-        setMemeImage(memeImageUrl)
+        setMeme(oldMeme => {
+            return {
+                ...oldMeme,
+                randomImage: memeImageUrl
+            }
+        })
     }
 
     return (
@@ -30,7 +40,7 @@ const Meme = () => {
 
                 <button onClick={getRandomMemeImage}>Get a new meme image 🖼</button>
             </div>
-            <img src={memeImage} className={styles['meme-image']} />
+            <img src={meme.randomImage} className={styles['meme-image']} alt="A random top 100 meme image"/>
         </main>
     )
 }
